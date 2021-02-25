9d22c2cdf650585ca6c84ed671c84574f5a1976d
Cornelis Romijn <Cornelis.Romijn@axiell.com>
2021-02-25 20:23:45 +0100

    done more exc

---
+++ PythonCrashCourse/chap2/number_eight.py
@@ -0,0 +1,6 @@
+print(5+3)
+print(11-3)
+print(2*4)
+print(int(64/8))
+print(89%9)
+print(2**3)
--- PythonCrashCourse/chap2/personal_message.py
+++ PythonCrashCourse/chap2/personal_message.py
@@ -1,3 +1,6 @@
+## Cornelis Romijn
+## This program prints a message to the person's name set in the person_name variable.
+
 person_name = "Cornelis"
 message = f"Hello {person_name}, would you like to learn some Python today?"
 print (message)
